1bf5d14a9a555b4f9825b511cf124cac7931b4d9
AdamBarreto <adambarreto0@gmail.com>
2025-08-02 14:50:19 -0300

    Criação de duas funções na classe Redes, 'config_rede()' e 'hospedar_partida()'. Respectivamente a primeira função, ela vai perguntar ao usuário qual a família do IP e qual o protocolo de rede a ser usado. Essa função tem uma estrutura que só vai permitir que o usuário continue se digitar corretamente a família do IP e o protocolo através do 'while True'. Além disso, cria-se um socket com essas duas informações e retorna o valor do socket 'sock', da familia do IP e do protocolo de rede para serem usados posteriormente, para isso utilizou-se o 'return'. Já na função 'hospedar_partida()' vai receber os parâmetros 'host', 'porta', 'sock', 'familia', 'protocolo'. Ele vai criar uma conexão com o host através do 'bind', em seguida ele verifica se é um host IPv4 ou IPv6, ou se der erro ele avisa. Para o protocolo TCP ele vai esperar uma conexão com um jogador e aceitar ela, pra garantir que os dispositivos estejam conectados. Ele também vai definir uma conexão específica om aquele computador com o 'conexao', que é um socket  específico para aquela conexão. Ele também vai receber o IP e a porta do cliente. Já caso seja UDP ele vai só simplesmente retornar o objeto 'sock', já que o protocolo UDP é conhecido pelo seu melhor esforço. Ambas as funções precisam de alguns ajustes, que é na questão de algumas condições. Ex.: alguém escolhe a familia IPv6, mas está numa rede IPv4.

--- Redes.py
+++ Redes.py
@@ -1,3 +1,59 @@
 import socket
 
-test = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#<--- Escolha das configurações de rede no menu --->
+def config_rede():
+  while True:
+    t_ip = input("Escolha a versão do protocolo de internet (IP), digite 'ipv4' ou 'ipv6': ").lower().strip()
+
+    if(t_ip == "ipv4"):
+      familia = socket.AF_INET
+      break                              #Uma pessoa pode escolher ipv6 mesmo sendo ipv4?
+    elif(t_ip == "ipv6"):
+      familia = socket.AF_INET6
+      break
+    else: print("-> Versão do IP inválida, digite exclusivamente 'ipv4' ou 'ipv6': ")
+
+  while True:
+    protc = input("Escolha o protocolo de rede, digite 'tcp' ou 'udp': ").lower().strip()
+    if(protc == "tcp"):
+      protocolo = socket.SOCK_STREAM
+      break
+    elif(protc == "udp"):
+      protocolo = socket.SOCK_DGRAM
+      break
+    else: print("-> Tipo de protocolo inválido, digite exclusivamente 'udp' ou 'tcp': ")
+
+  sock = socket.socket(familia, protocolo)
+  print("-> Socket criado com sucesso")
+  return familia, protocolo, sock
+
+#essa parte de baixo vai pro main
+familia, protocolo, sock = config_rede()
+
+#tenho que importar a biblioteca ipaddress
+#vai ser perguntado a pessoa a porta (1014-49151) e o seu IP
+def hospedar_partida(host, porta, sock, familia, protocolo):
+  try:
+    sock.bind((host, porta))
+    if (familia == socket.AF_INET):
+        print("Host IPv4 criado com sucesso. IP: {host} - Porta: {porta}")
+    else: print("Host IPv6 criado com sucesso. IP: {host} - Porta: {porta}")
+  except Exception as e:
+    print("Host não foi criado, insira o seu endereço IP correto e certifique-se de que a porta esteja na faixa de 1024-49151")
+
+  #<-- SE FOR TCP -->
+  if (protocolo == socket.SOCK_STREAM):
+    sock.listen(1)
+    print("Esperando conexão com um jogador [...]")
+
+    try:
+      conexao, (cliente_ip, cliente_porta) = sock.accept()
+      print("Servidor TCP conectado com IP {cliente_ip} da porta {cliente_porta}")
+      return conexao
+    except Exception as e:
+      print("Não foi possível estabelecer conexão com jogador")
+
+  #<-- SE FOR UDP -->
+  elif(protocolo == socket.SOCK_DGRAM):   #ajeitar
+    return sock
+    print("Servidor UDP pronto para receber mensagens")
